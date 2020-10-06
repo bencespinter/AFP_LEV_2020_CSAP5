8a1f119fd6ea9b3a467012ebee6ec3895f471093
Bence Pinter <pinter.bence.sandor@gmail.com>
2020-10-06 21:05:42 +0200

    Addigng background movement

---
+++ 2D_Game_Project_T5/Assets/BackgroundFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollow : MonoBehaviour
+{
+
+    public Transform followTransform;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(followTransform.position.x,0,0);
+
+
+    }
+}
