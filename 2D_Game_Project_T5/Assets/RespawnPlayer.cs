40c1a1cd383f015384f00764eea092c9747391c2
Bence Pinter <pinter.bence.sandor@gmail.com>
2020-12-06 18:12:19 +0100

    Added Background music and ambient sounds. Extended player level and background. Added the start of respawn collider.

---
+++ 2D_Game_Project_T5/Assets/RespawnPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnPlayer : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+            Application.LoadLevel(Application.loadedLevel);
+    }
+}
