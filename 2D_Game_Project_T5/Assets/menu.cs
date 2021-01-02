3c046068fc394bfc45d29b4fcaab10f5a8f08137
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-01-02 22:01:19 +0100

    Béka animálása
    Ellenséges béka animálása és mozgása

---
+++ 2D_Game_Project_T5/Assets/menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour 
+{
+	public void StartButton(string newGameLevel)
+	{
+		SceneManager.LoadScene (newGameLevel);	
+	}
+
+	public void ExitButton()
+	{
+		Application.Quit ();
+	}
+
+}
