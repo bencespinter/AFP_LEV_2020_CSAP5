748277431c52a718184806b4c60d4a03b8350a51
Csathó Zsolt <csatho.zsolt@gmail.com>
2021-01-07 16:54:32 +0100

    Karakter vezérlés módosítása

--- 2D_Game_Project_T5/Assets/menu.cs
+++ 2D_Game_Project_T5/Assets/menu.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour 
 {
+	public bool isStart;
+	public bool isQuit;
+	public bool isPause;
+	//void Start()
+	//   {
+
+	//   }
 	public void StartButton(string newGameLevel)
 	{
 		SceneManager.LoadScene (newGameLevel);	
 	}
-
+	void OnMouseUp()
+	{
+		if (isQuit)
+		{
+			Application.Quit();
+		}
+		if (isStart)
+		{
+			SceneManager.LoadScene("2nd_fields");
+			//Application.LoadLevel(Application.loadedLevel);
+			//Application.LoadLevel(1);			
+			//renderer.material.color = Color.cyan;
+		}
+	}
 	public void ExitButton()
 	{
 		Application.Quit ();
